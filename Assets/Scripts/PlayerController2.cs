aa5c2bdeead692ae82070a0811d45dd9afc14eea
Jadoncvs7 <141047708+Jadoncvs7@users.noreply.github.com>
2023-08-01 14:14:49 -0700

    enemie fixed buggs
    fixed bug wear player couildent grab object from left right and upwards
    
    // fixed bug wear player couldent throw objects not upwards
    
    // added enemie and basic enmie ai

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState { 
+    walk,
+    attack,
+    interact,
+    stagger,
+    idle
+}
+public class PlayerController2 : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+    private PickUp pickup;
+    public Vector4 startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+        startingPosition = new Vector4();
+        transform.position = startingPosition;
+        pickup = gameObject.GetComponent<PickUp>();
+        pickup.Direction = new Vector2(0, -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentState == PlayerState.interact)
+        {
+            return;
+        }
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change.sqrMagnitude > .1f)
+        {
+            pickup.Direction = change.normalized;
+        }
+    }
+}
